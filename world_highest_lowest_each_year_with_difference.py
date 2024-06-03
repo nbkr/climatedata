59222f8b7712a47ce275ad11fbdf30204366af4e
Ben Fleckenstein <bf@b1t.company>
2024-06-03 17:40:27 +0200

    Added with-difference script

---
+++ world_highest_lowest_each_year_with_difference.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python3
+
+import requests
+import sys
+
+
+data = requests.get('https://climatereanalyzer.org/clim/sst_daily/json/oisst2.1_world2_sst_day.json')
+
+result = {}
+for d in data.json():
+    if len(d['name']) != 4:
+        continue
+
+    if d['name'] not in result:
+        result[d['name']] = {}
+        result[d['name']]['highest'] = None
+        result[d['name']]['lowest'] = None
+
+    for e in d['data']:
+        if e is not None:
+            if result[d['name']]['highest'] == None or result[d['name']]['highest'] < e:
+                result[d['name']]['highest'] = e
+
+            if result[d['name']]['lowest'] == None or result[d['name']]['lowest'] > e:
+                result[d['name']]['lowest'] = e
+
+prevh = 0
+prevl = 0
+for key, value in sorted(result.items()):
+    if prevh == 0:
+        diffh = 0
+    else:
+        diffh = value['highest'] - prevh
+
+    if prevl == 0:
+        diffl = 0
+    else:
+        diffl = value['lowest'] - prevl
+
+    print ("{}\t{:.2f}\t{:+.2f}\t{:.2f}\t{:+.2f}".format(key,value['highest'], diffh, value['lowest'], diffl))
+
+    prevh = value['highest']
+    prevl = value['lowest']
